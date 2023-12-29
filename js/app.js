cd321fba2a1ee67bdb74044c7053cce63bfcfbb7
moonneer <122484250+MounirIssa@users.noreply.github.com>
2023-12-29 15:31:16 -0800

    new changes

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const sections = document.querySelectorAll('.section');
+const section_buttons = document.querySelectorAll('.controls');
+const section_button = document.querySelectorAll('.control');
+const all_sections = document.querySelector('.main-content');
+
+
+function PageTransitions() {
+
+    // Onclick -> event occurs (active-btn added to class):
+    for (let i = 0; i < section_button.length; i++) {
+        section_button[i].addEventListener('click', function() {
+            let current_button = document.querySelectorAll('.active-btn');
+            current_button[0].className = current_button[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+
+    // Section's active class:
+    all_sections.addEventListener('click', (e) => {
+        // Target the clicked element's data id:
+        const id = e.target.dataset.id;
+        if (id) {
+            section_buttons.forEach((button) => {
+                button.classList.remove('active');
+            });
+
+            e.target.classList.add('active');
+
+            // Hide other sections:
+            sections.forEach((section) => {
+                section.classList.remove('active');
+            });
+    
+            const element = document.getElementById(id);
+            element.classList.add('active');
+         }
+
+    })
+}
+
+PageTransitions();
